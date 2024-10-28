eb2361fc20daa7fa6ea7333b54a41f2206052f36
GautamKumar2005 <155357245+GautamKumar2005@users.noreply.github.com>
2024-10-29 00:39:59 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,11 +10,11 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/GautamKumar2005",
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+      "https://drive.google.com/sorry",
     linkedin: "https://www.linkedin.com/in/gautam-kumar-489903281/",
     twitter: " https://x.com/GautamK38839851",
     insta: "https://www.instagram.com/er.gautam___01",
-    facebook: "https://www.facebook.com/rishav.chanda.165/",
+    facebook: "https://www.facebook.com/I_am_sorry_i_had_not_uploaded_yet/",
   };
   
   export const skills = [
