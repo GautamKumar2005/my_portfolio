852b2a7d5e172ffce1743c2b391d8827e95644e2
GautamKumar2005 <gautam96544@gmail.com>
2026-05-29 17:57:23 +0530

    Fix Background3D maath TS error

--- components/Background3D.tsx
+++ components/Background3D.tsx
@@ -3,6 +3,7 @@
 import { useRef, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Text, Float } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 function ParticleSwarm(props: any) {
